c98bd79a1216af6fe7a3fc384edf3a211d6d8744
Lucas <rochaferreira19@gmail.com>
2024-10-02 20:32:17 -0300

    nota de aula 1

---
+++ Questao1/src/Main.java
@@ -0,0 +1,11 @@
+//Elabore um programa em java que informe se um aluno ele está aprovado, reprovado ou na final.
+//O sistema deve solicitar o nome e as notas do aluno.
+//Em seguida, realiza o cálculo da média e informa a situação acadêmica.
+//Se a média for >= 70 está aprovado, abaixo de 40 está reprovado, senão, está na final.
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Questao2/src/Main.java
@@ -0,0 +1,12 @@
+//Crie um sistema para controle de um produto com atributos código, nome, tamanho/peso, cor, valor e quantidade no estoque.
+//O sistema deve permitir o cadastro dos dados, além de realizar a venda e fazer o controle do estoque.
+//Para o pagamento, temos os seguintes benefícios:
+//Pix, Espécie, Transferência ou Débito, 5% de desconto.
+//Crédito, parcelar em 3x sem juros.
+//Observação: Espécie, caso o valor pago seja maior que o valor do produto, informar o troco.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
